6deee8c273fd7c9cbbbc9fbe3789f346ac3f204b
Bonvas <bonvas73@gmail.com>
2025-11-01 06:29:57 +0200

    Update a file exercise2_25.10

--- Basic Exercise for beginners/exercise2_25.10.py
+++ Basic Exercise for beginners/exercise2_25.10.py
@@ -8,11 +8,3 @@ for i in range(1, 11):
     x_sum = previous_num + i
     print("Current number", i, "Previous number ", previous_num, " Sum: ", x_sum)
     previous_num = i
-
-print("Printing current and previous number and their sum in a range(10)")
-previous_num = 0
-
-for i in range(1, 11):
-    x_sum = previous_num + i
-    print("Current number", i, "Previous number", previous_num, " Sum ", x_sum)
-    previous_num = i
